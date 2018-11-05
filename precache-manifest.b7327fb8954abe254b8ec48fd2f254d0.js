898b138f9b67bd6f9d416f3f2b67293289c1643d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-05 23:24:06 +0000

    Deploy alexgallardo/eatsheet to github.com/alexgallardo/eatsheet.git:gh-pages

---
+++ precache-manifest.b7327fb8954abe254b8ec48fd2f254d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eatsheet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b283c21e1e5b721c9a9d",
+    "url": "/eatsheet/static/js/runtime~main.b283c21e.js"
+  },
+  {
+    "revision": "c6d42cb3666432c22c1e",
+    "url": "/eatsheet/static/js/main.c6d42cb3.chunk.js"
+  },
+  {
+    "revision": "be11ca4053bbc83d9f71",
+    "url": "/eatsheet/static/js/1.be11ca40.chunk.js"
+  },
+  {
+    "revision": "c6d42cb3666432c22c1e",
+    "url": "/eatsheet/static/css/main.019c06e6.chunk.css"
+  },
+  {
+    "revision": "8bf0593cfdd489a378d7423767ac03bd",
+    "url": "/eatsheet/index.html"
+  }
+];
